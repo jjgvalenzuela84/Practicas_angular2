43446a15f85013d822382ea7f395f9a5cdd649df
jjgvalenzuela84 <jjgvalenzuela@gmail.com>
2017-09-13 18:54:35 +0200

    v1

---
+++ practica2/src/app/decorator/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['../decorator.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
